949376f09de13500bbd22344c3501cdda047c34c
Leonardo Condrasisen <leonardo.condrasisen@hotmail.com>
2026-04-23 10:31:41 -0300

    Fibonacci + controle de nome do arquivo gerado

--- Assemblador.py
+++ Assemblador.py
@@ -5,14 +5,28 @@
 #                   Modificado por: Leonardo Condrasisen                #
 #########################################################################
 
-#Adicionadas as instrucoes JMP, NOP, CMP, JNE, JL, JG ao dicionario
+# Modificacao 1: Adicionadas as instrucoes JMP, NOP, CMP, JNE, JL, JG ao dicionario
+# Modificacao 2: Arquivo de saida adicional em .txt com o mesmo conteudo do .cdm
+# Modificacao 3: Popup para nomear os arquivos de saida
+
+import tkinter as tk
+from tkinter import simpledialog
+
+root = tk.Tk()
+root.withdraw()
+
+output_name = simpledialog.askstring("Assemblador BIP", "Nome dos arquivos de saida (sem extensao):", initialvalue="asmimt")
+
+root.destroy()
+
+output_cdm  = (output_name or "asmimt") + ".cdm"
+output_txt  = (output_name or "asmimt") + ".txt"
 
 file1 = open('code_imt.txt', 'r')
 Lines = file1.readlines()
 
-result = open('asmimt.cdm', 'w')
-
-result_txt = open('asmimt.txt', 'w')
+result     = open(output_cdm, 'w')
+result_txt = open(output_txt, 'w')
 
 conversion = {"HLT":" : 0",
               "STO":" : 1",
@@ -38,7 +52,6 @@ for line in Lines:
         output = str(hex(n)).upper()[2:] + conversion[split[0]] + split[1] + "\n"
         result.write(output)
         result_txt.write(output)
-        print(line.strip())
         n = n+1
     except:
         pass
