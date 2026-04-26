e0b1b403b446b392058272f856b25daa40af3c07
Leonardo Condrasisen <leonardo.condrasisen@hotmail.com>
2026-04-26 13:39:34 -0300

    Otimizacao e nomes

--- Assemblador.py
+++ Assemblador.py
@@ -1,15 +1,20 @@
 #########################################################################
 #                   Prof. Nuncio Perrella                               #
 #                   Instituto Mauá de Tecnologia                        #
+#                   Sistemas Digitais e Arquitetura de Computadores     #
 #                   Assemblador para Processador BIP                    #
-#                   Modificado por: Leonardo Condrasisen                #
+#                                                                       #
+#                   João Carlos de Oliveira Serrano  - RA: 23.10205-5  #
+#                   Gabriel Tembra Gomes de Oliveira - RA: 22.10051-2  #
+#                   Gustavo Fernandes Arantes Maluta - RA: 23.10050-8  #
+#                   Leonardo Condrasisen             - RA: 23.01270-6  #
 #########################################################################
 
-# Modificacao 1: Adicionadas as instrucoes JMP, NOP, CMP, JNE, JL, JG ao dicionario
-# Modificacao 2: Arquivo de saida adicional em .txt com o mesmo conteudo do .cdm
-# Modificacao 3: Popup para nomear os arquivos de saida
-# Modificacao 4: Cria uma pasta com o nome escolhido e salva todos os arquivos dentro dela,
-#                incluindo copia do codigo fonte (_source.txt)
+# Modificação 1: Adicionadas as instruções JMP, NOP, CMP, JNE, JL, JG ao dicionário
+# Modificação 2: Arquivo de saída adicional em .txt com o mesmo conteúdo do .cdm
+# Modificação 3: Popup para nomear os arquivos de saída
+# Modificação 4: Cria uma pasta com o nome escolhido e salva todos os arquivos dentro dela,
+#                incluindo cópia do código fonte (_source.txt)
 
 import tkinter as tk
 from tkinter import simpledialog
@@ -18,13 +23,13 @@ import os
 root = tk.Tk()
 root.withdraw()
 
-output_name = simpledialog.askstring("Assemblador BIP", "Nome dos arquivos de saida (sem extensao):", initialvalue="asmimt")
+output_name = simpledialog.askstring("Assemblador BIP", "Nome dos arquivos de saída (sem extensão):", initialvalue="asmimt")
 
 root.destroy()
 
 output_name = output_name or "asmimt"
 
-# Modificacao 4: cria pasta com o nome escolhido
+# Modificação 4: cria pasta com o nome escolhido
 os.makedirs(output_name, exist_ok=True)
 
 output_cdm = os.path.join(output_name, output_name + ".cdm")
@@ -36,9 +41,9 @@ Lines = file1.readlines()
 
 result     = open(output_cdm, 'w')
 result_txt = open(output_txt, 'w')
-result_src = open(output_src, 'w')  # Modificacao 4: copia do codigo fonte
+result_src = open(output_src, 'w')  # Modificação 4: cópia do código fonte
 
-result_src.writelines(Lines)  # Modificacao 4: escreve o conteudo original
+result_src.writelines(Lines)  # Modificação 4: escreve o conteúdo original
 result_src.close()
 
 conversion = {"HLT":" : 0",
@@ -64,7 +69,7 @@ for line in Lines:
         split = line.strip().split(" ")
         output = str(hex(n)).upper()[2:] + conversion[split[0]] + split[1] + "\n"
         result.write(output)
-        result_txt.write(output)  # Modificacao 2: espelha saida no .txt
+        result_txt.write(output)  # Modificação 2: espelha saída no .txt
         print(line.strip())
         n = n+1
     except:
